4610033a712b7b8d42dfa5fd83d813231b4c7307
Ishak <vesilgame2006@gmail.com>
2024-09-29 19:34:39 +0300

    Add Zoo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 import org.example.Animals.*;
 
 public class Main {
-  public Main() {
-  }
 
   public static void main(String[] args) {
     Camel camel = new Camel("land", "Верблюжатина");
