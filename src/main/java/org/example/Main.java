75b395f0e8ee681c447cd1f8c3c8f7a725d54379
Ishak <vesilgame2006@gmail.com>
2024-10-05 20:16:23 +0300

    Add CustomArrayList, interface ArrayMethods Update gitignore

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,16 @@
 package org.example;
 
-import org.example.Animals.*;
+import org.example.CustomArrayList.CustomArrayList;
 
 public class Main {
-  public Main() {
-  }
-
   public static void main(String[] args) {
-    Camel camel = new Camel("land", "Верблюжатина");
-    Horse horse = new Horse("land", "Коник");
-    Owl owl = new Owl("flying", "Орленок");
-    Tiger tiger = new Tiger("land", "Тигренок");
-    Dolphin dolphin = new Dolphin("waterfowl", "Дельфинчик");
-    camel.move();
-    horse.move();
-    owl.move();
-    dolphin.move();
-    tiger.move();
-    System.out.println();
-    Grass grass = new Grass("укроп");
-    Fish fish = new Fish("карась");
-    Beef beef = new Beef("стейк");
-    Meet meet = new Meet("еж");
-    camel.eat(grass);
-    horse.eat(grass);
-    owl.eat(meet);
-    dolphin.eat(fish);
-    tiger.eat(beef);
+    CustomArrayList<Integer> array = new CustomArrayList<>();
+    array.add(1);
+    array.add(100);
+    System.out.println(array.get(1));
+    array.remove(1);
+    array.add(3);
+    array.get(10);
+
   }
 }
